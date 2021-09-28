686d038301ab3c59e5054956d28a2f1b203c90fa
tdninja1 <jmack23@uncc.edu>
2021-09-28 16:28:05 -0400

    Started calculator activity

---
+++ scripts/calculator_try.js
@@ -0,0 +1,29 @@
+
+//set up buttons 0-9 - not sure how to access
+//set up operators (+,-,/,*)
+//set up clear and decimal action values
+if (action === "add" || action === "subtract" || action === "multiply" || action === divide)
+{
+	String action;
+	//need to store action selected, not sure how
+	//log input
+	console.log("clickedItem = operator);
+}
+
+
+if (action === "decimal")
+{
+	//log input
+	console.log("clickedItem = decimal");
+}
+
+if (action === "clear")
+{
+	//log input
+	console.log("clickedItem = clear");
+}
+
+if (action === "calculate") 
+{
+	console.log("clickedItem = calculate");
+}
