a617576a95fc267ab438014d19e0c0170b357595
tdninja1 <jmack23@uncc.edu>
2021-11-03 15:12:12 -0400

    Completed most of my brand webpage for midterm.

---
+++ brand/scripts/default.js
@@ -0,0 +1,32 @@
+function displayContact() 
+{
+    let name = document.getElementById("enterName").value;
+    let phone = document.getElementById("enterPhoneNumber").value;
+    let email = document.getElementById("enterEmail").value;
+    let studentGrade = document.getElementById("studentGrade").value;
+    
+    
+    document.getElementById("formClass").innerHTML = 
+    "<div>" +
+
+    "<div>" +
+    "Name: " + name + 
+    "\n\n" + 
+    "</div>" +
+
+    "<div>" +
+    "Phone: \n" + phone +
+    "\n\n" +
+    "</div>" +
+
+    "<div>" +
+    "Email: \n" + email +
+    "\n\n" +
+    "</div>" +
+
+    "<div>" +
+    "Student Grade: \n" + studentGrade +
+    "</div>" +
+    
+    "</div>";
+}
