c577bd5c45b50d4297585d15778672bd43d1ae96
tdninja1 <jmack23@uncc.edu>
2021-11-12 15:30:53 -0500

    Finished Activity 11.

--- scripts/Mack-Activity11.js
+++ scripts/Mack-Activity11.js
@@ -1,4 +1,7 @@
-<!-- jQuery call to the accordion() method. -->
+// <!-- jQuery call to the accordion() method. -->
 $(document).ready(function() {
-    $("#tabs").tabs();
+    $("#tabs").accordion( {
+        heightStyle: 'content' //https://stackoverflow.com/questions/2841075/how-can-you-adjust-the-height-of-a-jquery-ui-accordion
+    });
+
 });
